32e8c8cee1e74add0ca056e5fa73919e15ecacfa
unknown <koalas8084@gmail.com>
2024-06-20 20:24:24 -0500

    Class 9 Studio

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,12 @@
 
 let launchcode = {
-  
-}
+  organization: "nonprofit",
+  executiveDirector: "Jeff",
+  percentageCoolEmployees: 100,
+ 
+
+};
+console.log(typeof( launchcode.percentageCoolEmployees));
 
 module.exports = launchcode;
 
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -2,7 +2,20 @@
 const launchcode = require('../index.js');
 
 describe("Testing launchcode", function(){
-
+  test("Should be organization: nonprofit", function() {
+    expect(launchcode.organization).toBe("nonprofit");
+  });
+  test("Should return executiveDirector: 'Jeff'", function() {
+    expect(launchcode.executiveDirector).toBe("Jeff");
+  });
+  test("Should return percentageCoolEmployees: '100'", function() {
+    expect(launchcode.percentageCoolEmployees).toBe("100");
+  });
+  test("Should return 'number' for typeof percerntageCoolEmployee", function() {
+    expect((typeof(launchcode.percentageCoolEmployees))).toBe("number");
+  });
   // Write your unit tests here!
   
 });
+
+
